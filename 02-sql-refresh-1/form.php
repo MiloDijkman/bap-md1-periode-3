62ba6a78c126e6f35fd3059cbe2496f033f9b540
Milo Dijkman <29147@ma-web.nl>
2020-02-26 14:37:13 +0100

    Opdracht 2 Klaar

---
+++ 02-sql-refresh-1/form.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+
+    <form action="process.php" method="post">
+
+      <p>
+        <label for="">Titel</label>
+        <input type="text" value="" name="titel">
+      </p>
+
+      <p>
+        <label for="">Artiest</label>
+        <input type="text" value="" name="artiest">
+      </p>
+
+      <p>
+        <label for="">Album</label>
+        <input type="text" value="" name="album">
+      </p>
+
+      <p>
+        <label for="">Duur</label>
+        <input type="text" value="" name="duur">
+      </p>
+
+      <p>
+        <label for="">Afbeelding</label>
+        <input type="url" value="" name="afbeelding">
+      </p>
+
+
+       <input type="submit" /></p>
+    </form>
+
+  </body>
+</html>
